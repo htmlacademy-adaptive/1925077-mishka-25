ef09f6cace52a9c5361bad2d460b948fdfaed0cd
ValeraGalera <nuberman88@gmail.com>
2022-05-01 16:14:30 +0400

    фикс на нескрытие элементов на таблетке и пк

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -6,19 +6,26 @@ menuButton.classList.remove('display-nope');
 firstList.classList.add('display-nope');
 secondList.classList.add('display-nope');
 
-menuButton.onclick = function () {
-  if (menuButton.classList.contains('main-header__menu-toggle--closed')) {
-    menuButton.classList.remove('main-header__menu-toggle--closed');
-    menuButton.classList.add('main-header__menu-toggle--open');
-    firstList.classList.remove('display-nope');
-    secondList.classList.remove('display-nope');
-    firstList.classList.add('main-header__first-list--absolutly');
-    secondList.classList.add('main-header__second-list--absolutly');
-  }
-  else {
-    menuButton.classList.add('main-header__menu-toggle--closed');
-    menuButton.classList.remove('main-header__menu-toggle--open');
-    firstList.classList.add('display-nope');
-    secondList.classList.add('display-nope');
-  }
-};
+
+
+if (window.screen.width >= 772) {
+  firstList.classList.remove('display-nope');
+  secondList.classList.remove('display-nope');
+} else {
+  menuButton.onclick = function () {
+    if (menuButton.classList.contains('main-header__menu-toggle--closed')) {
+      menuButton.classList.remove('main-header__menu-toggle--closed');
+      menuButton.classList.add('main-header__menu-toggle--open');
+      firstList.classList.remove('display-nope');
+      secondList.classList.remove('display-nope');
+      firstList.classList.add('main-header__first-list--absolutly');
+      secondList.classList.add('main-header__second-list--absolutly');
+    }
+    else {
+      menuButton.classList.add('main-header__menu-toggle--closed');
+      menuButton.classList.remove('main-header__menu-toggle--open');
+      firstList.classList.add('display-nope');
+      secondList.classList.add('display-nope');
+    }
+  };
+}
