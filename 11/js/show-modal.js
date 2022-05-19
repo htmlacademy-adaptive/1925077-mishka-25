0b8ffcf7c13ec2f0eed983328ee6a18d38224971
Keks <keks@htmlacademy.ru>
2022-05-19 14:47:53 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/show-modal.js
@@ -0,0 +1,1 @@
+let windowModal=document.querySelector(".modal-window__outer"),orderButton=document.querySelector(".product-of-week__button");orderButton.removeAttribute("href"),orderButton.onclick=function(){windowModal.classList.remove("modal-window__outer--none")},windowModal.onclick=function(){windowModal.classList.add("modal-window__outer--none")};
