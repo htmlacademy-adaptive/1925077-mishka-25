b8fe3826505f842e67560f0d67f2534932fb5eba
ValeraGalera <nuberman88@gmail.com>
2022-05-30 01:10:56 +0400

    b5. пофикшены ошибки именования классов

--- source/js/show-modal.js
+++ source/js/show-modal.js
@@ -1,11 +1,11 @@
-let windowModal = document.querySelector('.modal-window__outer');
+let windowModal = document.querySelector('.modal-window-wrapper');
 let orderButton = document.querySelector('.product-of-week__button');
 orderButton.removeAttribute('href');
 
 orderButton.onclick = function () {
-  windowModal.classList.remove('modal-window__outer--none');
+  windowModal.classList.remove('modal-window-wrapper--none');
 };
 
 windowModal.onclick = function () {
-  windowModal.classList.add('modal-window__outer--none');
+  windowModal.classList.add('modal-window-wrapper--none');
 };
